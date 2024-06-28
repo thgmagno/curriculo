132134b18d0b6da94505881767829e836be4e572
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-06-28 11:14:41 -0300

    add skills href

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -21,15 +21,51 @@ export function Skills() {
     <div className="flex flex-col text-sm sm:flex-row">
       <h2 className="mb-2 w-1/4 font-bold uppercase">Habilidades</h2>
       <div className="grid grid-cols-3 gap-2 text-xs sm:w-3/4 sm:text-sm md:text-base">
-        <Item imageSrc="/icons/node.svg" label="Node" href="#" />
-        <Item imageSrc="/icons/react.svg" label="React" href="#" />
-        <Item imageSrc="/icons/next.svg" label="Next.JS" href="#" />
-        <Item imageSrc="/icons/typescript.svg" label="Typescript" href="#" />
-        <Item imageSrc="/icons/sql.svg" label="SQL" href="#" />
-        <Item imageSrc="/icons/prisma.svg" label="Prisma" href="#" />
-        <Item imageSrc="/icons/api.svg" label="API Rest" href="#" />
-        <Item imageSrc="/icons/tailwind.svg" label="Tailwind" href="#" />
-        <Item imageSrc="/icons/html.svg" label="HTML/CSS/JS" href="#" />
+        <Item
+          imageSrc="/icons/node.svg"
+          label="Node"
+          href="https://nodejs.org/"
+        />
+        <Item
+          imageSrc="/icons/react.svg"
+          label="React"
+          href="https://react.dev/"
+        />
+        <Item
+          imageSrc="/icons/next.svg"
+          label="Next.JS"
+          href="https://nextjs.org/"
+        />
+        <Item
+          imageSrc="/icons/typescript.svg"
+          label="Typescript"
+          href="https://www.typescriptlang.org/"
+        />
+        <Item
+          imageSrc="/icons/sql.svg"
+          label="SQL"
+          href="https://www.postgresql.org/"
+        />
+        <Item
+          imageSrc="/icons/prisma.svg"
+          label="Prisma"
+          href="https://www.prisma.io/"
+        />
+        <Item
+          imageSrc="/icons/api.svg"
+          label="API Rest"
+          href="https://www.w3schools.in/restful-web-services/intro#google_vignette"
+        />
+        <Item
+          imageSrc="/icons/tailwind.svg"
+          label="Tailwind"
+          href="https://tailwindcss.com/"
+        />
+        <Item
+          imageSrc="/icons/html.svg"
+          label="HTML/CSS/JS"
+          href="https://www.w3schools.com/html/"
+        />
       </div>
     </div>
   )
